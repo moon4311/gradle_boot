0d092a5fbae796289bd31cca1d29386d8ab2cb07
kjm43 <kjm43@DESKTOP-VAKVRN1>
2019-09-06 12:37:14 +0900

    .properties -> .yml 변경 / mysql jdbc driver 추가

--- src/main/java/com/user/controller/UserController.java
+++ src/main/java/com/user/controller/UserController.java
@@ -4,9 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
-	@RequestMapping("/")
+	@RequestMapping("/main")
 	public String connectionTest() {
 		System.out.println("연결 확인");
 		return "Success Connect";
