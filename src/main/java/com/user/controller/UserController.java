576be5ff3a34df2ecf4079fb9d8f3419b8c92265
kjm43 <kjm43@DESKTOP-VAKVRN1>
2019-09-16 10:38:35 +0900

    vue.js test

--- src/main/java/com/user/controller/UserController.java
+++ src/main/java/com/user/controller/UserController.java
@@ -3,7 +3,6 @@ package com.user.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.user.model.UserVO;
 import com.user.service.UserService;
@@ -18,7 +17,6 @@ public class UserController {
 //	public @ResponseBody String connectionTest() {  return 값 그대로 출력
 	public String connectionTest() {		// return .jsp 로 이동
 		UserVO user = userService.getUser();
-		System.out.println("user : " + user.getUserId());
 		return "index";
 	}
 }
--- src/test/java/com/boot/GradleBootApplicationTests.java
+++ src/test/java/com/boot/GradleBootApplicationTests.java
@@ -1,9 +1,7 @@
 package com.boot;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 public class GradleBootApplicationTests {
